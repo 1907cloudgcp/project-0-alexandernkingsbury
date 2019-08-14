3ca9f42115c82c0a7f12cad70b8a06642591d259
alexandernkingsbury <53443343+alexandernkingsbury@users.noreply.github.com>
2019-08-14 09:45:33 -0400

    Add files via upload
    forgot a = sign which made me want to revise it again

--- commented/connection.py
+++ commented/connection.py
@@ -3,12 +3,28 @@ import logging
 import unittest
 import password 
 from datetime import date
+#handle zero balance
+myactualpasswordiuseforthings = password.password
 
 logging.basicConfig(filename = "project0.log", level = logging.DEBUG)
-    
+
+class queryFailure(ValueError):
+    pass
+
 class connectionWithDatabaseFailure(ValueError):
     pass
+
+class toMuchMoneyError(ValueError):
+    pass
     
+def valueCheck(quantity):
+    logging.debug("ValueCheck :" + str(quantity))
+    if(quantity >= 1000):
+        logging.error("ValueCheck :" + str(quantity))
+        raise toMuchMoneyError(str(quantity) + "this is more money then the server can hold")
+
+
+validConnection = True    
 
 def main():
     database = mysql.connector.connect(
@@ -65,15 +81,12 @@ def main():
 
         print("welcome " + sessionUser + " how might I help you today?")
 
-        #just decided to reuse loop without resetting it
         while(loop == False):
             print("Mode 1 manage balance")
             print("Mode 2 manage view history")
             print("Mode 3 quit")
-            #a basic query
             query = "SELECT balance FROM Client WHERE name = '" + sessionUser + "'"
             databaseCursor.execute(query)
-            #just easier to chop off the string for a awnser, just easier this way
             balanceOfUser = float((str(databaseCursor.fetchone()))[1:-2])        
             print("$" + str(balanceOfUser) + " is your current balance")
             mode = input()
@@ -81,16 +94,19 @@ def main():
                 print("Would you like to withdrawl? y/n")
                 action = input()
                 amountToWorkWith = 0
+                #take money from the user in the database
                 if(action == 'y'):
                     print("amount you would like to take out?")
                     amount = input()
                     balanceOfUser = float(balanceOfUser) - float(amount)
                     action = "took"
                     amountToWorkWith = amount
+                    #to prevent negative values
                     if(balanceOfUser < 0):
                         balanceOfUser = float(balanceOfUser) + float(amount)
                         print("sorry you can't have less then no money with this operation")
                         action = 1
+                #anything but y will add money to the  user
                 else:
                     action = "added"
                     print("amount you would like to add?")
@@ -98,69 +114,60 @@ def main():
                     amountToWorkWith = amount
                     balanceOfUser = float(balanceOfUser) + float(amount)
                     
-                valueCheck(balanceOfUser)
+                #if it is a valid action won't need to get here
                 if(action != 1):
                     print("add a description of the transaction")    
                     description = input()
-                    
                     description += " " + str(date.today()) + " amount " + action + " " + amountToWorkWith
                     print(description)
-                    
-                    query = "UPDATE Client SET balance = " + str(balanceOfUser) + "WHERE name = '" + sessionUser + "'"
-                    databaseCursor.execute(query)
-                    query = "INSERT INTO TransactionHistory(name, transaction) VALUES('" + sessionUser + "', '" + description + "')"
-                    databaseCursor.execute(query)
-                    print("Your balance is now "+ str(balanceOfUser))
-                    
+                    #try and execute the following queryies if they fail it is probably becuase money has gone over 1000
+                    try:
+                        query = "UPDATE Client SET balance = " + str(balanceOfUser) + "WHERE name = '" + sessionUser + "'"
+                        databaseCursor.execute(query)
+                        query = "INSERT INTO TransactionHistory(name, transaction) VALUES('" + sessionUser + "', '" + description + "')"
+                        databaseCursor.execute(query)
+                        print("Your balance is now "+ str(balanceOfUser))
+                    except: 
+                        logging.error("Query is invalid")
+                        valueCheck(balanceOfUser)
+                        raise queryFailure("query has failed")
+            #get the user's history
             elif(mode == '2'):
+                #query for this action
                 query = "SELECT * FROM TransactionHistory WHERE name = '" + sessionUser +"'"
-                if __name__ == '__main__':
-                    logging.debug("query failure")
-                    raise queryFailure("query has failed to execute")
-                
-                try:
-                    databaseCursor.execute(query)
-                except:
-                    if __name__ == '__main__':
-                        raise queryFailure("query has failed to execute")
-                    logging.debug("query failure")                    
+                databaseCursor.execute(query)
                 transactionHistory = databaseCursor.fetchall()
                 for action in transactionHistory:
                     print(str(action)[6 + len(sessionUser):-8] + "\n")
-                    
+            
+            #terminate the loop
             elif(mode == '3'):
                 print("have a good day")
                 loop = True
-
+        
+        #save changes
         database.commit()
 
-def valueCheck(quantity):
-    logging.debug("ValueCheck :" + str(quantity))
-    if(quantity > 500):
-        raise toMuchMoneyError(quantity)
-    if(quantity < 0):
-        raise toLittleMoneyError(quantity)
-
-
-validConnection = True   
-
+#to make sure connection is working
 def connectionTest():    
-    initialSetup = True
+    #try running the entire program
     try:
-        initialSetup = True
+        #initial setup
         database = mysql.connector.connect(
           host="localhost",
           user="root",
-          passwd=password.password,
+          passwd=myactualpasswordiuseforthings,
           database="testdatabase"
         )
-        validConnection = False
+        #so it stops running the entire file in the test file
         if __name__ == '__main__':
             main()
         return "working"
+    #if the database connection fails
     except:
         if __name__ == '__main__':
             raise connectionWithDatabaseFailure("make sure the database is running")
-        logging.debug("initial connection failure with database")
+        logging.error("initial connection failure with database")
+        return("failure")
 
 connectionTest()
